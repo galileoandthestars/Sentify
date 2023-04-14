89b40ce76d3d68e16579f9d306cf062aeed2860c
Fabiola Fonseca Rivera <fabiolafonsecarivera14@example.com>
2023-04-14 16:25:34 -0400

    Fixed Home import in App.js

--- src/App.js
+++ src/App.js
@@ -1,24 +1,14 @@
 import './App.css';
 import 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import {Home} from './Pages/Home';
 
-import { NavBar } from './components/NavBar';
-import {Banner} from './components/Banner'
-import {About} from './components/About';
-import {Features} from './components/Features';
-import {Creators} from './components/Creators';
-import {Contact} from './components/Contact';
+
 
 function App() {
   return (
 <div className="App">
-  <NavBar/>
-  <Banner/>
-  <About/>
-  <Features/>
-  <Creators/>
-  <Contact/>
-  
+  <Home/>
 </div>
   );
 }
--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,5 +1,6 @@
-import {NavBar} from '../components/NavBar';
+import {NavBar} from "../components/NavBar";
 import {About} from '../components/About';
+import {Banner} from '../components/Banner';
 import {Features} from '../components/Features';
 import {Creators} from '../components/Creators';
 import {Contact} from '../components/Contact';
@@ -8,6 +9,7 @@ export const Home = ()=>{
     return(
     <div className="home" id="/">
         <NavBar/>
+        <Banner/>
         <About/>
         <Features/>
         <Creators/>
