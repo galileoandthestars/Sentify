e72a54137e5c49bd920d31b5275eac42ea38c842
Fabiola Fonseca Rivera <fabiolafonsecarivera14@example.com>
2023-04-14 13:47:04 -0400

    Created SignUp form page

--- src/Pages/Signup.js
+++ src/Pages/Signup.js
@@ -1,9 +1,10 @@
 import React, { useState } from "react";
 
-export const Signup = (props) => {
+export const SignUp = (props) => {
     const [email, setEmail] = useState('');
     const [pass, setPass] = useState('');
     const [name, setName] = useState('');
+    const [confPass, setConfPass] = useState('');
 
     const handleSubmit = (e) => {
         e.preventDefault();
@@ -15,12 +16,19 @@ export const Signup = (props) => {
             <h2>Register</h2>
         <form className="register-form" onSubmit={handleSubmit}>
             <label htmlFor="name">Full name</label>
-            <input value={name} name="name" onChange={(e) => setName(e.target.value)} id="name" placeholder="full Name" />
-            <label htmlFor="email">email</label>
-            <input value={email} onChange={(e) => setEmail(e.target.value)}type="email" placeholder="youremail@gmail.com" id="email" name="email" />
-            <label htmlFor="password">password</label>
-            <input value={pass} onChange={(e) => setPass(e.target.value)} type="password" placeholder="********" id="password" name="password" />
-            <button type="submit">Log In</button>
+            <input value={name} name="name" onChange={(e) => setName(e.target.value)} id="name" placeholder="full Name"  required pattern="^[a-zA_ ]*$"/>
+            <span className="form-hint">Full name must include spaces!</span>
+            <label htmlFor="email">E-mail</label>
+            <input value={email} onChange={(e) => setEmail(e.target.value)}type="email" placeholder="youremail@gmail.com" id="email" name="email" required/>
+            <span className="form-hint">It should be a valid e-mail address!</span>
+            <label htmlFor="password">Password</label>
+            <input value={pass} onChange={(e) => setPass(e.target.value)} type="password" placeholder="********" id="password" name="password" required pattern="^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,20}$"/>
+            <span className="form-hint">Password should be 8-20 characters and include at least
+             <br/>1 letter, 1 number and 1 special character!</span>
+            <label htmlFor="password">Confirm Password</label>
+            <input value={confPass} onChange={(e) => setConfPass(e.target.value)} type="password" placeholder="********" id="confpassword" name="confpassword" required />
+            <span className="form-hint">Passwords don't match!</span>
+            <button className='submit-btn' type="submit">Sign Up</button>
         </form>
         <button className="link-btn" onClick={() => props.onFormSwitch('login')}>Already have an account? Login here.</button>
     </div>
