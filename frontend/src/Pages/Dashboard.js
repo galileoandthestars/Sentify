5c2e00aa45b9dc9664d46428ed80a0674079acc9
DiegoNogueras <dinogueras2001@gmail.com>
2023-04-17 15:54:15 -0400

    Added the useToken import and included a authentication guard that redirects to the Login page to set a token.

--- frontend/src/Pages/Dashboard.js
+++ frontend/src/Pages/Dashboard.js
@@ -1,6 +1,14 @@
-import React from "react";
+import React, { useState } from "react";
+import { Login } from "./Login";
+import useToken from "../components/useToken";
 
 export const Dashboard = () => {
+    const { token, setToken } = useToken();
+
+    if (!token) {
+        return <Login setToken={setToken} />
+    }
+
     return (
         <h1>Welcome</h1>
     );
