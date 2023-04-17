da4597bfcaf1d666d99ff72aabff3fb0fe8c35bd
DiegoNogueras <dinogueras2001@gmail.com>
2023-04-16 21:39:12 -0400

    Deleted the handleSubmit function, changed the name of some input tags, and deleted input requirements for quick debugging

--- frontend/src/Pages/Signup.js
+++ frontend/src/Pages/Signup.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import { Navbar } from "react-bootstrap";
 
@@ -8,11 +8,6 @@ export const SignUp = (props) => {
     const [name, setName] = useState('');
     const [confPass, setConfPass] = useState('');
 
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        console.log(email);
-    }
-
     return (
         <div className="register-page">
             <Navbar expand="lg">
@@ -20,19 +15,19 @@ export const SignUp = (props) => {
             </Navbar>
             <div className="auth-form-container">
                 <h2>Register</h2>
-                <form className="register-form" onSubmit={handleSubmit}>
-                    <label htmlFor="name" className="login-label">Full name</label>
-                    <input value={name} className="login-input" name="name" onChange={(e) => setName(e.target.value)} id="name" placeholder="full Name" required pattern="^[a-zA_ ]*$" />
+                <form className="register-form" method="post">
+                    <label htmlFor="name" className="login-label">Full Name</label>
+                    <input value={name} className="login-input" name="username" onChange={(e) => setName(e.target.value)} id="name" placeholder="Full Name" />
                     <span className="form-hint">Full name must include spaces!</span>
                     <label htmlFor="email" className="login-label">E-mail</label>
                     <input value={email} className="login-input" onChange={(e) => setEmail(e.target.value)} type="email" placeholder="youremail@gmail.com" id="email" name="email" required />
                     <span className="form-hint">It should be a valid e-mail address!</span>
                     <label htmlFor="password" className="login-label">Password</label>
-                    <input value={pass} className="login-input" onChange={(e) => setPass(e.target.value)} type="password" placeholder="********" id="password" name="password" required pattern="^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,20}$" />
+                    <input value={pass} className="login-input" onChange={(e) => setPass(e.target.value)} type="password" placeholder="********" id="password" name="password" />
                     <span className="form-hint">Password should be 8-20 characters and include at least
                         <br />1 letter, 1 number and 1 special character!</span>
                     <label htmlFor="password" className="login-label">Confirm Password</label>
-                    <input value={confPass} className="login-input" onChange={(e) => setConfPass(e.target.value)} type="password" placeholder="********" id="confpassword" name="confpassword" required />
+                    <input value={confPass} className="login-input" onChange={(e) => setConfPass(e.target.value)} type="password" placeholder="********" id="confpassword" name="confirmation_password" required />
                     <span className="form-hint">Passwords don't match!</span>
                     <button className='submit-btn' type="submit">Sign Up</button>
                 </form>
