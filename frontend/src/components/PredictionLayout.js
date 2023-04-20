86a5c7e6274824c62dbd28249778d7b9f063f84e
DiegoNogueras <dinogueras2001@gmail.com>
2023-04-19 22:22:46 -0400

    Changes allow for the recognized emotion to be displayed with changing colors.

--- frontend/src/components/PredictionLayout.js
+++ frontend/src/components/PredictionLayout.js
@@ -1,16 +1,58 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { CameraFeed } from './CameraFeed';
 
 export const PredictionLayout = () => {
+    const [emotion, setEmotion] = useState('')
+    const [icon, setIcon] = useState('');
+    const [emotionClass, setEmotionClass] = useState('');
+
+    /*Function that sets the emotion value, it is sent to 
+    the camera feed as a props */
+    const handlePhotoData = (emotion, status) => {
+        /*Checks if the takePhoto function was succesful */
+        if (status === 'success') {
+            setEmotion(emotion);
+        }
+        else {
+            setEmotion('no funciona');
+        }
+
+        if (emotion === 'happy') {
+            setEmotionClass('emotion-happy');
+        }
+        else if (emotion === 'sad') {
+            setEmotionClass('emotion-sad');
+        }
+        else if (emotion === 'angry') {
+            setEmotionClass('emotion-angry');
+        }
+        else if (emotion === 'fear') {
+            setEmotionClass('emotion-fear');
+        }
+        else if (emotion === 'neutral') {
+            setEmotionClass('emotion-neutral');
+        }
+        else if (emotion === 'disgust') {
+            setEmotionClass('emotion-disgust');
+        }
+        else if (emotion === 'surprise') {
+            setEmotionClass('emotion-surprise');
+        }
+        else {
+            console.log('Empty emotion value');
+        }
+    }
 
     return (
         <div className='predictionLayout'>
             <div className='predictionFeed'>
-                <CameraFeed />
+                {/*Handle photo data is passed as a prop called onPhotoData */}
+                <CameraFeed onPhotoData={handlePhotoData} />
             </div>
             <div className='predictionText'>
                 <h3>You are feeling </h3>
-                // function that calls get emotion
+                <img src={icon} alt="emotion-icon"></img>
+                <p className={emotionClass}>{emotion}</p>
             </div>
         </div>
     )
